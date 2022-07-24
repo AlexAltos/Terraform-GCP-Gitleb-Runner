e5b63d5ab6abccad234b3db773d81c3c60f7a4fb
Alex Nechaev <56437761+AlexAltos@users.noreply.github.com>
2022-07-24 20:08:43 +0600

    Update 9-gitlab_runner.tf

--- 9-gitlab_runner.tf
+++ 9-gitlab_runner.tf
@@ -7,17 +7,7 @@ module "gitlab-runner" {
   runner_tags               = var.gitlab_runner["tags"]
   runner_name               =  "${var.gitlab_runner["tags"]}-name"
 
-  # Pass annotations to service account. This can be for workload/pod/ identity
-  # service_account_annotations = {
-  #   "iam.gke.io/gcp-service-account" = module.workload_identity["gitlab-runner"].gcp_service_account_email
-  # }
-
-  # Use Local cache on Kubernetes nodes
-  #use_local_cache     = true
-
-  # Mount docker socket instead of using docker-in-docker
-  #mount_docker_socket = true
-
+  
   depends_on = [google_container_cluster.primary, google_container_node_pool.spot]
 }
 
